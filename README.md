04142a7db8698e942d4d504cd986b69dc3f732b4
wailhassan <wail.alammari@gmail.com>
2025-11-06 10:29:14 +0000

    Full open source forject

---
+++ README.md
@@ -0,0 +1,43 @@
+# 🥁 Drum Kit
+
+An interactive drum kit application that allows you to play drums using your keyboard or by clicking on the drum elements. This project demonstrates the use of JavaScript event handling, DOM manipulation, and audio playback.
+
+![Drum Kit Preview](./images/Preview.jpg)
+
+## Features
+
+- Play drum sounds using keyboard keys
+- Click-to-play drum elements
+- Visual feedback on drum hits
+- Multiple drum sounds including:
+  - Crash cymbal
+  - Kick bass
+  - Snare drum
+  - Various toms
+
+## How to Use
+
+1. Open `index.html` in your web browser
+2. Use your keyboard to press the corresponding keys shown on the drums
+3. Alternatively, click on the drum elements to play sounds
+4. Enjoy making your beats! 🎵
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript (ES6+)
+
+## Project Structure
+
+```
+├── index.html
+├── styles.css
+├── index.js
+├── images/
+└── sounds/
+```
+
+## Author
+
+Created by [WailHassan](https://github.com/wailhassan)
---
+++ index.js
@@ -0,0 +1,56 @@
+var x = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < x; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+function handleClick() {
+  var buttonInnerHTML = this.innerHTML;
+
+  switch (buttonInnerHTML) {
+    case "w":
+      var tom1 = new Audio("Sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("Sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("Sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("Sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("Sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("Sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick_bass = new Audio("Sounds/kick-bass.mp3");
+      kick_bass.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+    //   break;
+  }
+  // this.style.color = "white";
+  // console.log(this.innerHTML);
+  // console.log(this);
+  // alert("I got clicked");
+  // var audio = new Audio("sounds/tom-1.mp3");
+  // audio.play();
+}
